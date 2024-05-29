d763b7ddbfd788a27750d7e62dda1333fcc1d1c5
Cindy Singer <cindy_singer913@msn.com>
2024-05-29 08:39:59 +0200

    nouveau projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListeComponent } from './liste/liste.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [ListeComponent],
 })
-export class AppComponent {
-  title = 'tier-list-24';
-}
+export class AppComponent {}
